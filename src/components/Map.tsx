d3d75565217a11fe8acedc3228477e9702dc666e
Mathis Verleene <mathis.verleene@gmail.com>
2023-06-06 00:37:58 +0200

    Some update

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,63 +1,189 @@
-import React, { useEffect } from "react";
-import mapboxgl from "mapbox-gl";
-import "../styles/style.css";
+import * as React from 'react';
+import { useState, useMemo } from 'react';
+import { render } from 'react-dom';
+import Map, {
+  Marker,
+  Popup,
+  NavigationControl,
+  FullscreenControl,
+  ScaleControl,
+  GeolocateControl
+} from 'react-map-gl';
+import Card from './Card';
+
+interface City {
+  city: string;
+  temperature: number;
+  humidity: string;
+  image: string;
+  latitude: number;
+  longitude: number;
+}
+
 
 function MyMapComponent() {
-  useEffect(() => {
-    mapboxgl.accessToken =
-      "pk.eyJ1IjoidGltb3RoZWVyaW91IiwiYSI6ImNsaTdoNDEzNDFpOWozZG1sNzlhOXk3MnUifQ.gOtqKpJ4vQu4hMcq7Nclhg";
-    var map = new mapboxgl.Map({
-      container: "map",
-      //style: "mapbox://styles/mapbox/light-v11",
-      style: "mapbox://styles/mapbox/dark-v11",
-      center: [1.888334, 46.603354], // position de départ
-      zoom: 5, // niveau de zoom de départ
-    });
+  const [isLoading, setLoading] = useState(true);
+  const [markers, setMarkers] = useState<any | null>(null);
+  const [popupInfo, setPopupInfo] = useState<any | null>(null);
 
-    // Coordonnées pour la requête météo
-    // Coordonnées Lille
-    var lon = 3.0573;
-    var lat = 50.6292;
-
-    // Clé API OpenWeatherMap
-    //var apiKey = 'votre_cle_api';
-    var cities = ["Paris", "Lille", "Marseille", "Bordeaux", "Lyon", "Montpellier", "Toulouse", "Nantes", "Strasbourg", "Nice", "Brest",
+  var cities = ["Paris", "Lille", "Marseille", "Bordeaux", "Lyon", "Montpellier", "Toulouse", "Nantes", "Strasbourg", "Nice", "Brest",
     "Rennes", "Reims", "Toulon", "Grenoble", "Dijon", "Angers", "Nimes", "Saint-Denis", "Aix-en-Provence"];
-    
+
+
+
+  var CITIES: City[] = [];
+  var pins: any[] = [];
+  if (isLoading == true) {
     cities.forEach(city => {
-
-
-      // URL pour la requête API
-      var apiUrl = `http://localhost:3000/weather/city/`+city;
-
+      var apiUrl = `http://localhost:3000/weather/city/` + city;
       fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+          CITIES.push({
+            "city": data["city"],
+            "temperature": data["data"]["temperature"],
+            "humidity": data["data"]["humidity"],
+            "image": data["image"],
+            "latitude": data["lat"],
+            "longitude": data["lon"]
+          });
+        }).finally(() => {
+          console.log(CITIES);
+
+          pins =
+            CITIES.map((city, index) => (
+              <Marker
+                key={`marker-${index}`}
+                longitude={city.longitude}
+                latitude={city.latitude}
+                anchor="bottom"
+                color='#FFF'
+                onClick={e => {
+                  // If we let the click event propagates to the map, it will immediately close the popup
+                  // with `closeOnClick: true`
+                  e.originalEvent.stopPropagation();
+                  setPopupInfo(city);
+                }}
+              >
+                <p>{Math.round(city.temperature) + `°C`}</p>
+              </Marker>
+            ));
+          setMarkers(pins);
+          setLoading(false);
+        });
+    });
+  }
+
+  if (isLoading) {
+    return <div className="App">Loading...</div>;
+  } else {
+    console.log("pins", pins);
+  }
+  return (
+    <>
+      <Map
+        initialViewState={{
+          latitude: 46.603354,
+          longitude: 1.888334,
+          zoom: 3.5,
+          bearing: 0,
+          pitch: 0
+        }}
+        mapStyle="mapbox://styles/mapbox/dark-v9"
+        mapboxAccessToken="pk.eyJ1IjoidGltb3RoZWVyaW91IiwiYSI6ImNsaTdoNDEzNDFpOWozZG1sNzlhOXk3MnUifQ.gOtqKpJ4vQu4hMcq7Nclhg"
+      >
+        <GeolocateControl position="top-left" />
+        <FullscreenControl position="top-left" />
+        <NavigationControl position="top-left" />
+        <ScaleControl />
+
+        {markers}
+
+        {popupInfo && (
+          <Popup
+            anchor="top"
+            longitude={Number(popupInfo.longitude)}
+            latitude={Number(popupInfo.latitude)}
+            onClose={() => setPopupInfo(null)}
+          >
+            <Card
+              title="Température"
+              value={Math.round(popupInfo.temperature) + `°C`}
+              icon="src/img/icon_soleil.png"
+              valueMin="10°C"
+              valueMax="25°C"
+            />
+          </Popup>
+        )}
+      </Map>
+    </>
+  );
+}
+
+
+
+/*useEffect(() => {
+  const [showPopup, setShowPopup] = useState(false);
+  var cities = ["Paris", "Lille"];
+
+
+  cities.forEach(city => {
+    // URL pour la requête API
+    var apiUrl = `http://localhost:3000/weather/city/` + city;
+    fetch(apiUrl)
       .then(response => response.json())
       .then(data => {
-        // Crée un élément HTML pour le marqueur
-        var el = document.createElement("div");
-        el.className = "marker";
-        el.style.background = "transparent";
-        el.style.width = "5vw";
-        el.style.height = "5vh";
-        el.style.color = "white";
-        el.innerHTML = Math.round(data["data"]["temperature"])+`°C`;
-
-        lon = data["lon"];
-        lat = data["lat"];
-
-        // Crée un popup avec plus d'informations
-        var popup = new mapboxgl.Popup({ offset: 25 }).setHTML(
-          `<h3>`+data["city"]+`</h3><p>Température : `+data["data"]["temperature"]+`°C</p><p>Humidité : `+data["data"]["humidity"]+`%</p>`
-        );
-
-        // Crée le marqueur et l'ajoute à la carte
-        new mapboxgl.Marker(el)
-          .setLngLat([lon, lat])
-          .setPopup(popup) // Associe le popup au marqueur
-          .addTo(map);
+        console.log(data);
+        markers.push(
+          <Marker
+            key={`marker-${data["city"]}`}
+            longitude={data["lon"]}
+            latitude={data["lat"]}
+            anchor="bottom"
+            onClick={e => {
+              // If we let the click event propagates to the map, it will immediately close the popup
+              // with `closeOnClick: true`
+              e.originalEvent.stopPropagation();
+              setShowPopup(data["city"]);
+            }}
+          >
+            <p>{Math.round(data["data"]["temperature"]) + `°C`}</p>
+          </Marker>
+        )
       });
-    });
   });
-  return <div id="map" style={{ width: "100%", height: "100%" }}></div>;
-}
+
+  console.log(markers);
+
+  return <Map
+    initialViewState={{
+      longitude: 1.888334,
+      latitude: 46.603354,
+      zoom: 4,
+    }}
+    mapStyle="mapbox://styles/mapbox/dark-v11"
+    mapboxAccessToken="pk.eyJ1IjoidGltb3RoZWVyaW91IiwiYSI6ImNsaTdoNDEzNDFpOWozZG1sNzlhOXk3MnUifQ.gOtqKpJ4vQu4hMcq7Nclhg"
+  >
+    {markers}
+
+    {/* {showPopup && (
+    <Popup longitude={-100} latitude={40}
+      anchor="bottom"
+      onClose={() => setShowPopup(false)}>
+      You are here
+
+      <Card
+        title="Température"
+        value="21°C"
+        icon="src/img/icon_soleil.png"
+        valueMin="10°C"
+        valueMax="25°C"
+      />
+    </Popup>)} *//*}
+</Map>;
+});
+return <div id="map" style={{ width: "100%", height: "100%" }}></div>;
+}*/
+
 export default MyMapComponent;
